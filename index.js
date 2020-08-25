960f843ae392157b1d9901cd926286afaa4e5379
kathryn0908 <kathrynlael@gmail.com>
2020-08-25 17:40:36 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+const main = document.querySelector('#main')
+main.remove();
+
+
+const newHeader = document.createElement('h1')
+newHeader.id = "victory"
+newHeader.innerHTML = "Katie is the champion"
+document.body.append(newHeader, newHeader, newHeader)
+
 // Write your code here!
